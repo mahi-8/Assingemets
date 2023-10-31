e824781dacbb120d5a4f6370724c27e5387633a7
mahi-8 <144072351+mahi-8@users.noreply.github.com>
2023-10-31 04:41:03 -0700

    first cooments

---
+++ index.ts
@@ -0,0 +1,12 @@
+
+// Excercise 2
+
+/*Personal Message: Store a person’s name in a variable, and print a message to 
+that person. Your message should be simple, such as, “Hello Eric, would you like 
+to learn some Python today?
+*/
+
+// Personal Message
+
+let  personName :string="Maheera";
+console.log(`Hello ${personName},would you like to learn some pyhthon today`);
